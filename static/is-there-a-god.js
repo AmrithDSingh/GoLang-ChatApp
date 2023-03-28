781dd2eed7d1d1c270511c27771b8a9d065ef09f
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-03-28 10:07:48 +0100

    working go server

--- static/is-there-a-god.js
+++ static/is-there-a-god.js
@@ -1,21 +1,81 @@
-//Initialise question field
+// Initialise question field
 let questions = [];
 
-//Create 'form' to link html to js, selects the html form
+// Create 'form' to link html to js, selects the html form
 const form = document.querySelector('.form');
 
-//Create the list of questions and links to html
+// Create the list of questions and links to html
 const questionList = document.querySelector('#question-list');
 
 // Function to render the list of questions
 function renderQuestions() {
   // Clear the existing list
   questionList.innerHTML = '';
-  
+
   // Create a new list item for each question
   questions.forEach((question) => {
     const listItem = document.createElement('li');
-    listItem.innerText = question.title;
+    const title = document.createElement('h3');
+    const content = document.createElement('p');
+    const answerForm = document.createElement('form');
+    const answerInput = document.createElement('input');
+    const answerButton = document.createElement('button');
+    
+    title.innerText = question.title;
+    content.innerText = question.content;
+    answerInput.setAttribute('type', 'text');
+    answerInput.setAttribute('placeholder', 'Type your answer here:');
+    answerButton.innerText = 'Submit';
+    
+    answerForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const answer = answerInput.value.trim();
+      if (answer) {
+        question.answers.push({
+          id: Date.now(),
+          content: answer,
+          votes: 0
+        });
+        answerInput.value = '';
+        renderQuestions();
+      }
+    });
+
+    listItem.appendChild(title);
+    listItem.appendChild(content);
+    
+    const answerList = document.createElement('ul');
+    answerList.setAttribute('class', 'answer-list');
+    
+    question.answers.forEach((answer) => {
+      const answerItem = document.createElement('li');
+      const answerContent = document.createElement('p');
+      const voteForm = document.createElement('form');
+      const voteButton = document.createElement('button');
+      const voteCount = document.createElement('span');
+      
+      answerContent.innerText = answer.content;
+      voteButton.innerText = 'Vote';
+      voteCount.innerText = answer.votes.toString();
+      
+      voteForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        answer.votes += 1;
+        voteCount.innerText = answer.votes.toString();
+      });
+      
+      voteForm.appendChild(voteButton);
+      voteForm.appendChild(voteCount);
+      answerItem.appendChild(answerContent);
+      answerItem.appendChild(voteForm);
+      answerList.appendChild(answerItem);
+    });
+    
+    listItem.appendChild(answerList);
+    listItem.appendChild(answerForm);
+    answerForm.appendChild(answerInput);
+    answerForm.appendChild(answerButton);
+    
     questionList.appendChild(listItem);
   });
 }
@@ -40,3 +100,14 @@ form.addEventListener('submit', (event) => {
     renderQuestions();
   }
 });
+
+// Get profile data from local storage, or create an empty object if none exists
+let profileData = JSON.parse(localStorage.getItem('profileData')) || {};
+
+// Create variables for profile form elements
+const profileForm = document.querySelector('#profile-form');
+const profileNameInput = document.querySelector('#name');
+const profileEmailInput = document.querySelector('#email');
+const profilePicInput = document.querySelector('#profile-pic');
+const profilePicPreview = document.querySelector('.profile-pic');
+
