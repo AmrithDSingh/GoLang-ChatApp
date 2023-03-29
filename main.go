8ac42274c637ac23c8a18e8048a44668ecb1fef9
Samuel Barlow <samuelbarlow11@gmail.com>
2023-03-29 16:15:44 +0100

    html

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	fmt.Print("test")
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	
+
 		if err := template.ExecuteTemplate(w, "let-us-see.html", welcome); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
